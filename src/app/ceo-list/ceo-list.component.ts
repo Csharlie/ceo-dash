4fa240d452598aa43d42c9a3ec83627314a09bdb
Csharlie <petersardy@gmail.com>
2024-11-16 23:16:32 +0100

    query data from component

--- src/app/ceo-list/ceo-list.component.ts
+++ src/app/ceo-list/ceo-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CeoService } from '../services/ceo.service';
 
 @Component({
   selector: 'app-ceo-list',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './ceo-list.component.html',
   styleUrl: './ceo-list.component.css'
 })
-export class CeoListComponent {
 
+export class CeoListComponent implements OnInit {
+  ceos: any[] = [];
+
+  constructor(private ceoService: CeoService) {}
+
+  ngOnInit() {
+    this.ceos = this.ceoService.getCeos();
+  }
 }
